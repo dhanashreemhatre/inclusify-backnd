c859593cbeeb3d636b2ba9884455b6fc877c0801
Dhanashree Mhatre <93194086+dhanashreemhatre@users.noreply.github.com>
2024-01-10 12:33:03 +0530

    Delete inclusify-backend directory

--- inclusify-backend/store/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import All_products
-
-
-urlpatterns=[
-    path("products",All_products.as_view(),name="products"),
-]
--- inclusify-backend/vendor/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Vendor(models.Model):
-    name=models.CharField(max_length=255,default="vendor")
-    address=models.TextField(default="this is my address")
-    
-    
